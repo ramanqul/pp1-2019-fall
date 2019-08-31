04db3a555213b2d3a8e42aca2279fc310560d97d
Ramanqul <buzaubakov.raman@gmail.com>
2019-08-31 16:28:43 +0600

    lab1:a

---
+++ 01_week/lab/ex_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	
+	cin >> a;
+	cin >> b;
+	
+	double h = sqrt(a*a + b*b);
+	
+	cout << h;
+	
+	return 0;
+}
