be3514137064a401cbbb6bd11db869fc6dc49f82
Ramanqul <buzaubakov.raman@gmail.com>
2019-09-14 12:26:30 +0600

    week 3: upd

---
+++ 03_week/infinite_until_loop.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x;
+	int sum = 0;
+	while (cin >> x) {
+		
+		sum = sum + x;
+		
+	}
+	
+	cout << sum << endl;
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
