68b1a24d1d6ee3638dc4852161293b73f1c4ef8e
Ramanqul <buzaubakov.raman@gmail.com>
2019-09-21 12:18:09 +0600

    week 4:sort

---
+++ 04_week/sort_test.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+//1,2,-4,10,8,7
+//become: -4,1,2,7,8,10
+int main() {
+	
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i=0;i<n;i++) {
+		cin >> a[i];
+	}
+	
+	for (int i=0;i<n;i++) {
+		for (int j=0;j<n;j++) {
+			
+			if (a[i] < a[j]) {
+				int tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+			
+		}
+	}
+	
+	for (int i=0;i<n;i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	
+	
+}
+
+
+
+
+
+
+
+
